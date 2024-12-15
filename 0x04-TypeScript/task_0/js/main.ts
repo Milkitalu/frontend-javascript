11b28dd20adc884d68008bef5306c2781652b017
Talaku Getahun <getahuntalaku@gmail.com>
2024-12-15 17:08:27 +0300

    Code for main.ts is created according stated direction

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,49 @@
+// Definition of the Student interface.
+interface Student {
+    firstName:string;
+    lastName:string;
+    age:number;
+    location:string;
+}
+
+//Creating tow Students.
+const student_1: Student = {
+    firstName: "Talaku",
+    lastName: "Getahun",
+    age: 27,
+    location: "Addis Abeba"
+
+};
+
+const student_2: Student = {
+    firstName: "Ayantu",
+    lastName: "Getahun",
+    age: 20,
+    location: "Adama"
+
+};
+
+//Creating an array containing the two students
+
+const studentsList: Student[] =[student_1, student_2];
+
+// creating Rendering table using vanilla JS
+
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+
+studentsList.forEach(student =>{
+    const row = document.createElement('tr');
+    const firstNameCell = document.createElement('td');
+    const locationCell = document.createElement('td');
+
+    firstNameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+
+    row.appendChild(firstNameCell);
+    row.appendChild(locationCell);
+    tbody.appendChild(table);
+});
+
+table.appendChild(tbody);
+document.body.appendChild(table);
