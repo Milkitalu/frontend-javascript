fb7da88b45d451f86496fb0f352573442486a63b
Talaku Getahun <getahuntalaku@gmail.com>
2024-12-15 20:27:47 +0300

    Creating functions specific to employees project

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -55,3 +55,25 @@ function createEmployee(salary: number | string): Director | Teacher {
   console.log(createEmployee(200)); // Output: Teacher
   console.log(createEmployee(1000)); // Output: Director
   console.log(createEmployee('$500')); // Output: Director
+
+  // isDirector Function Definition
+
+  function isDirector(employee: Director | Teacher): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+  }
+
+  // executeWork Function Definition
+
+  function executeWork(employee: Director | Teacher): void{
+    if (isDirector(employee)) {
+        console.log(employee.workDirectorTasks());
+    } else {
+        console.log(employee.workTeachersTasks());
+    }
+  }
+
+const employee1 = createEmployee(200);
+executeWork(employee1); // Output: Getting to work
+
+const employee2 = createEmployee(1000);
+executeWork(employee2); // Output: Getting to director tasks
