1c69176a323c56172eb4d745ae13ac016bd5d20c
Talaku Getahun <getahuntalaku@gmail.com>
2024-12-15 19:41:18 +0300

    Directors interface that extends teacher functionality is created

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -49,47 +49,8 @@ table.appendChild(tbody);
 document.body.appendChild(table);
 
 
-// 1.Building Teacher interface.
-
-interface Teacher {
-    readonly firstName:string;
-    readonly lastName:string;
-    fullTimeEmployee:boolean;
-    yearsOfExperience ?:number;
-    location:string;
-    [key:string]:any;   // to add any other attribute
-}
 
 
-// Creating Teacher object
-
-const teacher3: Teacher = {
-    firstName:"John",
-    lastName:"Doe",
-    fullTimeEmployee:false,
-    location:"Adama",
-    contract:true //
-};
-
-console.log(teacher3);
-
-
-// An interface for directors that extends teachers functionality
-
-interface Directors extends Teacher {
-    numberOfReports: number;
-  }
-  
-  // Creating a Directors object.
-
-const director: Directors = {
-    firstName: "Jane",
-    lastName: "Smith",
-    fullTimeEmployee: true,
-    location: "Los Angeles",
-    numberOfReports: 5,
-    contract: true // Additional attribute
-  };
 
   // Function that 
 
@@ -111,3 +72,6 @@ interface printTeacherFunction {
 
 const printT: printTeacherFunction = printTeacher;
 console.log(printT("John", "Doe")); 
+
+//Writing Class tasks
+
